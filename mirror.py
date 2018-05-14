af1575c7a0c18c3c07052c6002890c061860cafc
Markgraf, Marco (415) <Marco.Markgraf@daimler.com>
2018-05-14 16:49:30 +0200

    aaaand I should output valid xml instead...

--- mirror.py
+++ mirror.py
@@ -1,33 +1,32 @@
 '''
 This script fetches a rss-feed, downloads images contained in it to a local directory
 and writes a new feed-file that serves the images from the local directory.
-
-Useful to embed e.g. an ebay-feed into your site without forwarding your users
-IP-adress to ebay...
 '''
-import codecs
 import urllib.request
 import feedparser
+import fileinput
 from bs4 import BeautifulSoup
 
-d = feedparser.parse('https://some_feed_you_want_to_mirror/feed')
-f = str(d)
+original_feed = 'https://somehost/somedir/feed'
+urllib.request.urlretrieve(original_feed, 'tempdir' + '/feed.xml')
+
+local_prefix = 'https://yourhost/somedir/'
+d = feedparser.parse(original_feed)
+
 images = []
 
-# find url of images
+# find url of images and fetch them to local dir
 for item in d['entries']:
     soup = BeautifulSoup(item['summary'], 'html.parser')
     img = soup.select('img')
-    images.extend([i['src'] for i in img if  i['src']])
-
-# download file to temp-dir
-for image in images:
-    localfile = 'tempdir/' + image.split('/')[-2] + '_' + image.split('/')[-1]
-    #print('saving ' + str(image) + ' to ' + str(localfile))
-    urllib.request.urlretrieve(image, localfile)
-    # rewrite the original feed to use local files
-    f = f.replace(str(image), str(localfile))
-
-outfile = codecs.open('new_feed.rss', 'w', 'utf-8')
-outfile.write(f)
-outfile.close()
+    images = [i['src'] for i in img if  i['src']]
+    for image in images:
+        localfile = 'tempdir/' + image.split('/')[-2] + '_' + image.split('/')[-1]
+        newurl = str(local_prefix) + str(localfile)
+        # print('saving ' + str(image) + ' to ' + str(localfile))
+        urllib.request.urlretrieve(image, localfile)
+        # and now we search and replace in the file...
+        # instead of importing a library to generate rss-feeds...
+        with fileinput.FileInput('tempdir/feed.xml', inplace=True, backup='.bak') as file:
+            for line in file:
+            print(line.replace(image, newurl), end='')
